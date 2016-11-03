be892e4e53cdb157e5c15373c1c2fcfbe31cd8a1
Pedro Saratscheff <psaratscheff@gmail.com>
2016-11-03 19:07:42 -0300

    ssl hotfixes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'app_token': '9c00e472-c119-431e-97fa-019a30ebc783',
     'transactions_url': 'https://alquitran.ing.puc.cl/transactions/',
-    'server_url': 'http://localhost:8083/arquicoins/'
+    'server_url': 'https://assw9.ing.puc.cl/arquicoins/'
 };
