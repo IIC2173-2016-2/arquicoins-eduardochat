00694af7df7055a2be538b3d854b5e3553dcb849
Pedro Saratscheff <psaratscheff@gmail.com>
2016-11-12 17:06:45 -0300

    update cassandra contact points

--- arquicoins/services/cassandra.js
+++ arquicoins/services/cassandra.js
@@ -3,7 +3,7 @@ const cassandra = require('express-cassandra');
 function createClient() {
     var models = cassandra.createClient({
         clientOptions: {
-            contactPoints: ['127.0.0.1'],
+            contactPoints: ['146.155.13.130', '146.155.13.131', '146.155.13.132'],
             protocolOptions: { port: 9042 },
             keyspace: 'arquicoins',
             queryOptions: {consistency: cassandra.consistencies.one}
